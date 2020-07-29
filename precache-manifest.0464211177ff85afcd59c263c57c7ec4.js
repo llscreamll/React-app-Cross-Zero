406a78f20339345ca076b1e44bff8f0ee751a988
Scream <skate_max@mail.ru>
2020-07-29 10:28:36 +0300

    Updates

---
+++ precache-manifest.0464211177ff85afcd59c263c57c7ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35de711537a9701336ff370e143c0759",
+    "url": "/React-app-Cross-Zero/index.html"
+  },
+  {
+    "revision": "9f58c44caa28e0c6b927",
+    "url": "/React-app-Cross-Zero/static/css/main.57b74b6a.chunk.css"
+  },
+  {
+    "revision": "c4019a92addae3c2979f",
+    "url": "/React-app-Cross-Zero/static/js/2.0b9c6b91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-Cross-Zero/static/js/2.0b9c6b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f58c44caa28e0c6b927",
+    "url": "/React-app-Cross-Zero/static/js/main.dcdff8f6.chunk.js"
+  },
+  {
+    "revision": "23350b590c25654ed8a9",
+    "url": "/React-app-Cross-Zero/static/js/runtime-main.6197a65d.js"
+  }
+]);
